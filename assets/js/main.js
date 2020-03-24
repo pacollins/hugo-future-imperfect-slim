c928a312ab8265994ca64eacf692e517cc3703d7
pacollins <thepatrickcollins@gmail.com>
2020-03-24 00:28:03 -0400

    Add slideshow js and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,30 @@ $('#back-to-top').click(function() {
   $('html, body').animate({scrollTop: 0}, 1000);
   return false;
 });
+
+// Featured slideshow
+var slideIndex = 1;
+showSlides();
+
+function currentSlide(n) {
+  var test = n;
+  showSlides(slideIndex = n-1);
+  setTimeout(showSlides, 7000);
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slideshow-slide");
+  var dots = document.getElementsByClassName("slideshow-indicator");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
+}
