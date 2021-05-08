b54ad2a64b9b42906b0986810cabe0b762c3bd7e
Patrikck COllisn <patrick@pacollins.com>
2021-05-08 19:21:29 -0400

    Change Module Loading and Restructure User Add-Ons

---
+++ exampleSite/assets/js/user-footer.js
@@ -0,0 +1,6 @@
+/*
+  This js file is for individual users to add scripts to the footer of their
+  personal site, or for the implementation of features specifically for their site.
+  Anything that is an official part of the theme (ex. Pull Requests) should be
+  included in main.js and follow the formatting and style given.
+*/
