23b921b4c2d0a6c7b8a9fabf174fbfc873b3e792
Patrick Collins <patrick@pacollins.com>
2021-07-25 11:53:40 -0400

    Remove jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,216 @@
+var initPhotoSwipeFromDOM = function(gallerySelector) {
+  var images = document.querySelectorAll('.content-body .art');
+  for (i = 0; i < images.length; i++) {
+    const img = new Image();
+    img.src = images[i].getAttribute('href');
+    images[i].setAttribute("data-size", img.width+'x'+img.height);
+  }
+  // parse slide data (url, title, size ...) from DOM elements
+  // (children of gallerySelector)
+  var parseThumbnailElements = function(el) {
+    var thumbElements = el.querySelectorAll('.frame'),
+        numNodes = thumbElements.length,
+        items = [],
+        figureEl,
+        linkEl,
+        size,
+        item;
+
+    for(var i = 0; i < numNodes; i++) {
+
+      figureEl = thumbElements[i]; // <figure> element
+      // include only element nodes
+      if(figureEl.nodeType !== 1) {
+        continue;
+      }
+
+      linkEl = figureEl.children[0]; // <a> element
+
+      size = linkEl.getAttribute('data-size').split('x');
+
+      // create slide object
+      item = {
+        src: linkEl.getAttribute('href'),
+        w: parseInt(size[0], 10),
+        h: parseInt(size[1], 10)
+      };
+
+
+
+      if(figureEl.children.length > 1) {
+        // <figcaption> content
+        item.title = figureEl.children[1].innerHTML;
+      }
+
+      if(linkEl.children.length > 0) {
+        // <img> thumbnail element, retrieving thumbnail url
+        item.msrc = linkEl.children[0].getAttribute('src');
+      }
+
+      item.el = figureEl; // save link to element for getThumbBoundsFn
+      items.push(item);
+    }
+
+    return items;
+  };
+
+  // find nearest parent element
+  var closest = function closest(el, fn) {
+    return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+  };
+
+  // triggers when user clicks on thumbnail
+  var onThumbnailsClick = function(e) {
+    e = e || window.event;
+
+    var eTarget = e.target || e.srcElement;
+
+    // find root element of slide
+    var clickedListItem = closest(eTarget, function(el) {
+      return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+    });
+
+    if(!clickedListItem) {
+      return;
+    }
+
+    // find index of clicked item by looping through all child nodes
+    // alternatively, you may define index via data- attribute
+    var clickedGallery = clickedListItem.parentNode,
+        childNodes = clickedListItem.parentNode.querySelectorAll('figure'),
+        numChildNodes = childNodes.length,
+        nodeIndex = 0,
+        index;
+
+    for (var i = 0; i < numChildNodes; i++) {
+      if(childNodes[i].nodeType !== 1) {
+        continue;
+      }
+
+      if(childNodes[i] === clickedListItem) {
+        index = nodeIndex;
+        break;
+      }
+      nodeIndex++;
+    }
+
+
+
+    if(index >= 0) {
+      // open PhotoSwipe if valid index found
+      openPhotoSwipe( index, clickedGallery );
+    }
+    return false;
+  };
+
+  // parse picture index and gallery index from URL (#&pid=1&gid=2)
+  var photoswipeParseHash = function() {
+    var hash = window.location.hash.substring(1),
+    params = {};
+
+    if(hash.length < 5) {
+      return params;
+    }
+
+    var vars = hash.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      if(!vars[i]) {
+        continue;
+      }
+      var pair = vars[i].split('=');
+      if(pair.length < 2) {
+        continue;
+      }
+      params[pair[0]] = pair[1];
+    }
+
+    if(params.gid) {
+      params.gid = parseInt(params.gid, 10);
+    }
+
+    return params;
+  };
+
+  var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+    var pswpElement = document.querySelectorAll('.pswp')[0],
+        gallery,
+        options,
+        items;
+
+    items = parseThumbnailElements(galleryElement);
+
+    // define options (if needed)
+    options = {
+
+      // define gallery index (for URL)
+      galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+      getThumbBoundsFn: function(index) {
+        // See Options -> getThumbBoundsFn section of documentation for more info
+        var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+            pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+            rect = thumbnail.getBoundingClientRect();
+
+        return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+      }
+    };
+
+    // PhotoSwipe opened from URL
+    if(fromURL) {
+      if(options.galleryPIDs) {
+        // parse real index when custom PIDs are used
+        // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+        for(var j = 0; j < items.length; j++) {
+          if(items[j].pid == index) {
+              options.index = j;
+              break;
+            }
+        }
+      } else {
+        // in URL indexes start from 1
+        options.index = parseInt(index, 10) - 1;
+      }
+    } else {
+      options.index = parseInt(index, 10);
+    }
+
+    // exit if index not found
+    if( isNaN(options.index) ) {
+      return;
+    }
+
+    if(disableAnimation) {
+      options.showAnimationDuration = 0;
+    }
+
+    // Pass data to PhotoSwipe and initialize it
+    gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  };
+
+  // loop through all gallery elements and bind events
+  var galleryElements = document.querySelectorAll( gallerySelector );
+
+  for(var i = 0, l = galleryElements.length; i < l; i++) {
+    galleryElements[i].setAttribute('data-pswp-uid', i+1);
+    galleryElements[i].onclick = onThumbnailsClick;
+  }
+
+  // Parse URL and open gallery if it contains #&pid=3&gid=1
+  var hashData = photoswipeParseHash();
+  if(hashData.pid && hashData.gid) {
+    openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+  }
+};
+
+// execute above function
+initPhotoSwipeFromDOM('.content-body');
+
 const menus = [
   { button: "search-button", menu: "search" },
   { button: "lang-button", menu: "lang-menu" },
-  { button: "share-button", menu: "share-menu"} ,
-  { button: "nav-button", menu: "site-nav-menu"}
+  { button: "share-button", menu: "share-menu"},
+  { button: "nav-button", menu: "site-nav"}
 ];
 
 function closeMenus(openMenu) {
@@ -17,19 +225,37 @@ for (const {button, menu} of menus) {
   var buttons = document.getElementsByClassName(button);
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function() {
+      var toggles = document.querySelectorAll('.toggle:not(.nav-button)');
       var menuClasses = document.getElementById(menu).classList;
       var wrapperClasses = document.getElementById("wrapper").classList;
       if (menuClasses.contains('active')) {
         menuClasses.remove('active');
         wrapperClasses.remove('overlay');
+        for (let t = 0; t < toggles.length; t++) {
+          toggles[t].classList.remove('active');
+          toggles[t].classList.remove('inactive');
+        }
       } else {
         menuClasses.add('active');
         wrapperClasses.add('overlay');
+        for (let t = 0; t < toggles.length; t++) {
+          if (toggles[t].classList.contains(button)) {
+            toggles[t].classList.remove('inactive');
+            toggles[t].classList.add('active');
+          } else {
+            toggles[t].classList.remove('active');
+            toggles[t].classList.add('inactive');
+          }
+        }
+        if (menu == "search") {
+          document.getElementById("search-input").focus();
+        }
       }
       closeMenus(menu);
     };
   }
 }
+
 
 document.onclick = function(e){
   var path = e.composedPath().includes(document.getElementById('site-header'));
@@ -39,22 +265,31 @@ document.onclick = function(e){
   }
 };
 
-// Check to see if the window is top if not then display button
-$(window).scroll(function() {
-  if ($(this).scrollTop()) {
-    $('#back-to-top').fadeIn();
-  } else {
-    $('#back-to-top').fadeOut();
+document.addEventListener("keydown", (e) => {
+  var wrapper = document.getElementById("wrapper").classList;
+  if (e.key === "Escape" && wrapper.contains("overlay")) {
+    wrapper.remove('overlay');
+    closeMenus();
   }
 });
 
-// Click event to scroll to top
-$('#back-to-top').click(function() {
-  $('html, body').animate({scrollTop: 0}, 1000);
-  return false;
-});
+var backToTop = document.getElementById("back-to-top");
 
-// Search
+window.onscroll = function () {
+  if (window.scrollY == 0) {
+    backToTop.classList.remove('active');
+  } else {
+    backToTop.classList.add('active');
+  }
+};
+
+backToTop.onclick = function(){
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+};
+
 var idx = null;         // Lunr index
 var resultDetails = []; // Will hold the data for the search results (titles and summaries)
 var $searchResults;     // The element on the page holding search results
@@ -87,7 +322,10 @@ window.onload = function () {
       idx = lunr(function () {
         this.ref('ref');
         this.field('title');
-        this.field('data');
+        this.field('timestamp');
+        this.field('author');
+        this.field('readtime');
+        this.field('date');
         this.field('description');
         this.field('body');
 
@@ -97,18 +335,21 @@ window.onload = function () {
             this.add(doc);
             resultDetails[doc.ref] = {
               'title': doc.title,
+              'timestamp': doc.timestamp,
+              'author': doc.author,
+              'readtime': doc.readtime,
               'date': doc.date,
               'description': doc.description,
             };
         }, this);
       });
     } else {
-      $searchResults.innerHTML = '<article class="mini-post"><main><p>Error loading search results...</p></main></a></article>';
+      $searchResults.innerHTML = '<article class="content post"><div class="content-body"><p>Error loading search results...</p></div></article>';
     }
   };
 
   request.onerror = function() {
-    $searchResults.innerHTML = '<article class="mini-post"><main><p>Error loading search results...</p></main></a></article>';
+    $searchResults.innerHTML = '<article class="content post"><div class="content-body"><p>Error loading search results...</p></div></article>';
   };
 
   // Send the request to load the JSON
@@ -130,7 +371,7 @@ function registerSearchHandler() {
     if ($searchInput.value == '') {
       $searchResults.innerHTML = '';
     }
-  }
+  };
 }
 
 function renderSearchResults(results) {
@@ -139,7 +380,7 @@ function renderSearchResults(results) {
   if (results.length > 0) {
     results.forEach(function(result) {
       // Create result item
-      container.innerHTML += '<article class="mini-post"><a href="' + result.ref + '"><header><h2>' + resultDetails[result.ref].title + '</h2><time class="published" datetime="">' + resultDetails[result.ref].date + '</time></header><main><p>' + resultDetails[result.ref].description + '</p></main></a></article>';
+      container.innerHTML += '<article class="content post"><a href="' + result.ref + '"><header class="content-header"><div class="content-summary"><h1 class="content-title">' + resultDetails[result.ref].title + '</h1><p class="content-description">' + resultDetails[result.ref].description + '</p></div><div class="content-meta"><time class="content-date" datetime="' + resultDetails[result.ref].timestamp + '">' + resultDetails[result.ref].date + '</time><p class="post-author">' + resultDetails[result.ref].author + '</p><p class="post-time">' + resultDetails[result.ref].readtime + '</p></div></header></a></article>';
     });
 
     // Remove any existing content so results aren't continually added as the user types
@@ -149,7 +390,7 @@ function renderSearchResults(results) {
       );
     }
   } else {
-    $searchResults.innerHTML = '<article class="mini-post"><main><p>No Results Found...</p></main></a></article>';
+    $searchResults.innerHTML = '<article class="content post"><div class="content-body"><p>Results Not Found...</p></div></article>';
   }
 
   // Render the list
